6f8a3634db58507beba30821eb9cfb50d1886ced
Eduardo-Barraque <eduabarraque@gmail.com>
2021-12-01 19:17:44 -0300

    Site totalmente funcional,mas com erro no terminal

--- app.py
+++ app.py
@@ -1,8 +1,1 @@
-from flask import render_template
-from application.model.dao.produtoDao import ProdutoDao
 from application import app
-
-@app.route("/", methods=['GET'])
-def home():
-    produtos = ProdutoDao().produtosBusca()
-    return render_template("home.html", produtos = produtos)
--- application/__init__.py
+++ application/__init__.py
@@ -3,3 +3,5 @@ import os
 
 app = Flask(__name__,static_folder=os.path.abspath("application/view/static"), 
             template_folder=os.path.abspath("application/view/templates"))
+
+from application.controller import home_controller
